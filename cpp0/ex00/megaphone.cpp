bbfa7874c55169133ab95ab8e3411a056ad87cfc
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-09-13 19:08:16 +0200

    coucou

--- cpp0/ex00/megaphone.cpp
+++ cpp0/ex00/megaphone.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
-int main()
+int main(int argc, char **argv)
 {
-	std::cout << "bonjour" << std::endl;
+	int	idx = 0;
+	int	argv_idx = 0;
+
+	while (argv_idx < argc)
+	{
+		while(argv[argv_idx][idx])
+		{
+			argv[argv_idx][idx] = toupper(argv[argv_idx][idx]);
+			idx++;
+		}
+		idx = 0;
+		argv_idx++;
+	}
+	idx = 1;
+	if (argc > 1)
+	{
+		while (idx < argc)
+		{
+			std::cout << argv[idx];
+			idx++;
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
 	return (0);
 }
