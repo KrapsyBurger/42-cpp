49959cb328e9057e0d7cc166aa8282474dba23d2
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-09-19 15:37:25 +0200

    coucou

--- cpp0/ex02/Account.cpp
+++ cpp0/ex02/Account.cpp
@@ -1,12 +1,21 @@
 #include "Account.hpp"
 #include <iostream>
 
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
 Account::Account(int initial_deposit)
 {
+	std::cout << "je suis dans le constructor initial deposit";
 	std::cout << initial_deposit << std::endl;
 }
 
-Account::Account() {}
+Account::Account()
+{
+	std::cout << "je suis le constructor" << std::endl;
+}
 
 Account::~Account() {}
 
@@ -33,4 +42,35 @@ int	Account::getNbWithdrawals()
 void Account::displayAccountsInfos()
 {
 	std::cout << _nbAccounts << std::endl;
+}
+
+void Account::makeDeposit(int deposit)
+{
+	std::cout << deposit << std::endl;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	std::cout << withdrawal << std::endl;
+	return (0);
+}
+
+int Account::checkAmount() const
+{
+	std::cout << "je suis checkAmount" << std::endl;
+	return (0);
+}
+
+void Account::displayStatus() const
+{
+	std::cout << this->_accountIndex << std::endl;
+	std::cout << this->_amount << std::endl;
+	std::cout << this->_nbDeposits;
+	std::cout << this->_nbWithdrawals;
+	std::cout << "je suis displayStatus" << std::endl;
+}
+
+void Account::_displayTimestamp()
+{
+	std::cout << "je suis displayTimestamp" << std::endl;
 }
