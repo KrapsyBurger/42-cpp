403dcb42f6d4f74e9fc67ddaff983752e13479c7
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-10-13 20:48:22 +0200

    coucou

---
+++ cpp7/ex00/main.cpp
@@ -0,0 +1,14 @@
+#include "whatever.hpp"
+
+int main()
+{
+	int a = 2;
+	int b = 3;
+	
+	// swap(a, b);
+	// std::cout <<  a << std::endl;
+
+	std::cout << min(a, b) << std::endl;
+	std::cout << max(a, b) << std::endl;
+	return (0);
+}
