587370f06b5d2abb33c04aad6c2d4e36b611810c
Nathan FASCIA <nfascia@e1r4p2.clusters.42paris.fr>
2022-10-19 12:11:01 +0200

    coucou

--- cpp2/ex01/main.cpp
+++ cpp2/ex01/main.cpp
@@ -1,4 +1,5 @@
 #include "Fixed.hpp"
+#include "color.hpp"
 
 int main()
 {
@@ -14,10 +15,19 @@ int main()
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
 
+	std::cout << LBLUE;
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << STOP;
 	
+	std::cout << LRED;
+	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+	std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+	std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+	std::cout << "d is " << d.toFloat() << " as float" << std::endl;
+	std::cout << STOP;
+
 	return (0);
 }
