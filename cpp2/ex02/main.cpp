1f85defbb90b7891c2d6492043798ded73b8c3de
Nathan FASCIA <nfascia@e1r4p2.clusters.42paris.fr>
2022-10-19 12:15:44 +0200

    coucou

--- cpp2/ex02/main.cpp
+++ cpp2/ex02/main.cpp
@@ -1,19 +1,24 @@
 #include "Fixed.hpp"
+#include "color.hpp"
 
 int main()
 {
 	Fixed		a;
 	Fixed const	b(Fixed(5.05f) * Fixed(2));
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
+	std::cout << CYAN;
+	std::cout << "a initial value : " << a << std::endl;
+	std::cout << "pre-incr : " << ++a << std::endl;
+	std::cout << "a value : " << a << std::endl;
+	std::cout << "post incr : " << a++ << std::endl;
+	std::cout << "a value : " << a << std::endl;
 
-	std::cout << b << std::endl;
+	std::cout << "b initial value : " << b << std::endl;
+	std::cout << STOP;
 
-	std::cout << Fixed::max(a, b) << std::endl;
-	
+	std::cout << MAGENTA;
+	std::cout << "max between a and b : " << Fixed::max(a, b) << std::endl;
+	std::cout << STOP;
+
 	return (0);
 }
